74907549edfd5580fb32009588c4fd0ac85ee14b
María Reneé Morales Mijangos <99778960+MariaReneeMorales@users.noreply.github.com>
2022-03-16 11:44:51 -0600

    Librerías + Título

---
+++ Universidad.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <iostream> 
+#include <math.h> 
+#include <string.h> 
+using namespace std;
+
+int main(){
+	//Permite imprimir tildes, ñ o caracteres especiales. 
+	setlocale(LC_ALL, "");
+	
+	//Título.
+	cout << endl << endl;
+	cout << "          U N I V E R S I D A D  X Y Z";
+	cout << endl << endl;
+	
+	return 0;
+}
